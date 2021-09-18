6d064acbbe49136e27751a1fda9867cf301ae84d
giwig <giwig2014@gmail.com>
2021-09-18 23:14:11 +0300

    - project created

--- main.py
+++ main.py
@@ -1,31 +1,40 @@
+import os.path
 import sys
 
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QRadioButton, QSizePolicy
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QRadioButton, QSizePolicy, QFileDialog
 from hexview import HexViewWidget
 from ui.mainwindow import Ui_MainWindow
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
-    buf: bytearray = 0
+    buf: bytearray = bytearray(0)
 
     def __init__(self):
         super(MainWindow, self).__init__()
-        # self.ui = Ui_MainWindow()
-        # self.ui.setupUi(self)
         self.setupUi(self)
 
-        with open("main.py") as f:
-            self.buf = f.read()
+        self.hv = HexViewWidget(self.buf)
+        self.hv.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.hv.setParent(self.gbRight)
+        self.verticalLayout.addWidget(self.hv)
+        self.binding()
 
-        btn = QPushButton("LALALA")
-        btn.setParent(self.gbRight)
-        self.verticalLayout.addWidget(btn)
+    def binding(self):
+        self.actionOpen.triggered.connect(self.on_open_action)
 
-        hv = HexViewWidget(self.buf)
-        hv.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        hv.setParent(self.gbRight)
-        self.verticalLayout.addWidget(hv)
+    def on_open_action(self):
+        print("Open")
+        file, check = QFileDialog.getOpenFileNames(None, 'Open file...', '.')
+        if check:
+            print(file[0])
+            if os.path.isfile(file[0]):
+                with open(file[0]) as f:
+                    self.buf = f.read()
+                self.hv.close()
+                self.hv = HexViewWidget(self.buf)
+                self.hv.setParent(self.gbRight)
+                self.verticalLayout.addWidget(self.hv)
 
 
 app = QApplication(sys.argv)
